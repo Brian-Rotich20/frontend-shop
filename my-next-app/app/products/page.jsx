1f5cbeff759f3b4e69ffbc262ad1d191aaf0dd27
Brian-Rotich20 <ralphrotich@gmail.com>
2025-07-18 21:49:47 +0300

    removed page

--- my-next-app/app/products/page.jsx
+++ my-next-app/app/products/page.jsx
@@ -1,24 +1,24 @@
-//products list
-import ProductList from '@components/ProductList';
+// //products list
+// import ProductList from '@components/ProductList';
 
 
-async function getProducts() {
-  const res = await fetch('https://django-shop-drf.onrender.com/product_list',  {
-    cache: 'no-store',
-  });
-  if (!res.ok) return [];
-  const data = await res.json();
-  // If your API returns { results: [...] }
-  return Array.isArray(data) ? data : data.results || [];
-}
+// async function getProducts() {
+//   const res = await fetch('https://django-shop-drf.onrender.com/product_list',  {
+//     cache: 'no-store',
+//   });
+//   if (!res.ok) return [];
+//   const data = await res.json();
+//   // If your API returns { results: [...] }
+//   return Array.isArray(data) ? data : data.results || [];
+// }
 
-export default async function ProductsPage() {
-  const products = await getProducts();
+// export default async function ProductsPage() {
+//   const products = await getProducts();
 
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">All Products</h1>
-      <ProductList products={products} />
-    </main>
-  );
-}
+//   return (
+//     <main className="p-6">
+//       <h1 className="text-2xl font-bold mb-4">All Products</h1>
+//       <ProductList products={products} />
+//     </main>
+//   );
+//}
