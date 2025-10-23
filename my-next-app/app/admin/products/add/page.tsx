6737db2d80afadbdc71b77b99c3c2b790b7a67ce
Brian-Rotich20 <ralphrotich@gmail.com>
2025-10-23 14:45:48 +0300

    add product page

---
+++ my-next-app/app/admin/products/add/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+export default function AddProduct() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Add Products</h1>
+      <p>This is a product page.</p>
+    </div>
+  );
+}                   
